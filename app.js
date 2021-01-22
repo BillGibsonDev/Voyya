b258d7a3f76474c3eeb437359a266282c0dbe55a
wgibson17 <71801512+wgibson17@users.noreply.github.com>
2021-01-22 16:14:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ function initMap() {
         zoom: 12,
     });
 }
+
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    limit: 12,
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJWOTVvdzhISkRPWjVlUWttNWpZAOXFPSi1oZAGNNbmNhQXo4MW9rdUszOUlYZAXJBamlTbGRUY24zODhEZADlDYW1VbEEzOE82dmM3c1NSN1ZADYjhnc2VWQ3QwQTJaWjhRRXI2SUhJNGJqNUptNTJCTgZDZD'
+});
+userFeed.run();
